026cfb779c107b135d77ba9a8ae17e861240eeec
Eric Brelsford <ebrelsford@gmail.com>
2015-04-23 16:31:59 -0400

    Include more information in the instructions
    There is more data for each feature, include street and facility in addition
    to the class.

--- scripts/city_not_osm.py
+++ scripts/city_not_osm.py
@@ -2,4 +2,12 @@
 Provide instructions for adding a bike lane given its properties.
 """
 def instruction(properties):
-    return 'Add this class %s bike lane' % properties['facilitycl']
+    return "Add this class %s bike lane on %s. The city's data says it is %s." % (
+        properties['facilitycl'],
+        properties['street_1'],
+        get_facility(properties).lower(),
+    )
+
+
+def get_facility(properties):
+    return '/'.join(filter(None, [properties['ft_facilit'], properties['tf_facilit']]))

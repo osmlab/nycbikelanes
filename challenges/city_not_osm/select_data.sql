4ee4a3fa861d25083c18f74b4e65489742c9348f
Eric Brelsford <ebrelsford@gmail.com>
2016-09-06 13:38:12 -0400

    Merge lines by street name, class

--- challenges/city_not_osm/select_data.sql
+++ challenges/city_not_osm/select_data.sql
@@ -1,3 +1,4 @@
-SELECT nyclines.*
-FROM nyclines, osmlines_buffer
-WHERE NOT ST_within(nyclines.wkb_geometry, osmlines_buffer.wkb_geometry)
+SELECT ST_linemerge(ST_collect(city.wkb_geometry)) AS wkb_geometry, street, boro, bikedir, ft_facilit, tf_facilit, facilitycl
+FROM nyclines city, osmlines_buffer osm_buffer
+WHERE NOT ST_within(city.wkb_geometry, osm_buffer.wkb_geometry)
+GROUP BY street, boro, bikedir, ft_facilit, tf_facilit, facilitycl
